2e00029fea5857844ad0ed4a3857379102957f27
truonghoangthuan <tthun32@gmail.com>
2021-01-13 03:07:47 +0700

    13/01/2021 3h06

--- thuansite/polls/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('detail/<int:question_id>', views.detailView, name='detail'),
-    path("list/", views.viewList, name='view_list'),
-    path('', views.index)
-]
